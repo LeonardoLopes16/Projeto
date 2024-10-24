7b76a4da86aca5e0bca154fee5c21978408c1333
Leonardo Lopes <leuuu1010@gmail.com>
2024-10-24 20:22:04 -0300

    utilizando zip e zip_longest, e também desenvolvendo exercicio

---
+++ ex.py
@@ -0,0 +1,11 @@
+lista_a = [1,2,3,4,5,6,7]
+lista_b = [1,2,3,4]
+def soma(lista_a,lista_b):
+    a = list(zip(lista_a,lista_b))
+    re = []
+    i=0
+    for it in a:
+        re.append(lista_a[i] + lista_b[i])
+        i+=1
+    print(re)
+soma(lista_a,lista_b)
