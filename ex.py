f2fa4d5bdb3adff96d2616ee7fafe446767693b8
Leonardo Lopes <leuuu1010@gmail.com>
2024-10-28 13:11:25 -0300

    Utilizando algumas funções, como GrupeBy, Map e Count

--- ex.py
+++ ex.py
@@ -1,11 +1,13 @@
 lista_a = [1,2,3,4,5,6,7]
 lista_b = [1,2,3,4]
-def soma(lista_a,lista_b):
-    a = list(zip(lista_a,lista_b))
-    re = []
-    i=0
-    for it in a:
-        re.append(lista_a[i] + lista_b[i])
-        i+=1
-    print(re)
-soma(lista_a,lista_b)
+# def soma(lista_a,lista_b):
+#     a = list(zip(lista_a,lista_b))
+#     re = []
+#     i=0
+#     for it in a:
+#         re.append(lista_a[i] + lista_b[i])
+#         i+=1
+#     print(re)
+# soma(lista_a,lista_b)
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)
